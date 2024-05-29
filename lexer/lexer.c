3dc5c122e3c9c53f931e33d029c72fe1d83f66cf
Antoine Demaille <ademaill@2D1.42angouleme.fr>
2024-05-29 10:40:12 +0200

    lexer

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: ademaill <ademaill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 14:01:37 by ademaill          #+#    #+#             */
-/*   Updated: 2024/05/23 16:22:44 by ademaill         ###   ########.fr       */
+/*   Updated: 2024/05/28 17:09:44 by ademaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ int	ft_token_join(t_token *src, t_token *add, int j)
 	return (0);
 }
 
-static int	ft_group_cmd(t_token *token)
+/*static int	ft_group_cmd(t_token *token)
 {
 	t_token	*tmp;
 	t_token	*tmp2;
@@ -55,7 +55,7 @@ static int	ft_group_cmd(t_token *token)
 	//if (regroup(token, tmp, tmp2, tmp3) == 1)
 		//gestion d'erreur
 	return (0);
-}
+}*/
 
 void	token_type(t_token *token, t_main *main)
 {
@@ -71,41 +71,6 @@ void	token_type(t_token *token, t_main *main)
 		token->type = __cmdgr;
 }
 
-/* char	*ft_sort(char **tab, int i)
-{
-	int	j;
-	char	*str;
-	bool	first_is_cmd;
-	bool	first_is_special;
-
-	str = NULL;
-	j = 0;
-	first_is_cmd = false;
-	first_is_special = false;
-	while (tab[i + j])
-	{
-		if (ft_strncmp(tab[i + j], "|", 1) == 0)
-			break;
-		if ((ft_strncmp(tab[i + j], ">" ,1) == 0 || ft_strncmp(tab[i + j], "<" ,1) == 0 || ft_strncmp(tab[i + j], ">>" ,2) == 0 || ft_strncmp(tab[i + j], "<<" ,2) == 0) && !first_is_cmd)
-		{
-			if (j == 0)
-				first_is_special = true;
-			if (tab[i + 1])
-			{
-				str = ft_strjoin(ft_strjoin(tab[i], " "), tab[i + 1]);
-				return (str);
-			}
-		}
-		else
-		{
-			if (j == 0)
-				first_is_cmd = true;
-			str = ft_strjoin(str, tab[i + j]);
-		}
-		j++;
-	}
-	return (str);
-} */
 
 void	ft_sort(t_main *main)
 {
@@ -135,16 +100,83 @@ void	ft_sort(t_main *main)
 		token = token->next;
 	}
 }
+char	**creat_tab(char **src)
+{
+	char	**tab;
+	int		j;
+	int		i;
+
+	j = 2;
+	i = 0;
+	tab = malloc(sizeof (char *) * ft_len_tab(src) - j + 1);
+	if (!tab)
+		return (NULL);
+	while (src[j])
+	{
+		tab[i] = src[j];
+		src[j] = NULL;
+		i++;
+		j++;
+	}
+	tab[i] = NULL;
+	return (tab);
+}
+
+void	ft_no_cmd(t_main *main)
+{
+	t_token	*tmp;
+	int		cmd_count;
+	t_token	*tmp2;
+
+	cmd_count = 0;
+	tmp = main->token;
+	while (tmp)
+	{
+		if (tmp->type == __cmdgr)
+			cmd_count++;
+		if (tmp->type == __redirect_in || tmp->type == __redirect_out)
+		{
+			tmp2 = tmp;
+			while (tmp2)
+			{
+				if (tmp2->type == __cmdgr)
+					cmd_count++;
+				tmp2 = tmp2->next;
+			}
+			if (cmd_count == 0)
+				ft_new_node(&tmp, creat_tab(tmp->value));
+		}
+		tmp = tmp->next;
+	}
+}
+
+static void	ft_find_type(t_main *main)
+{
+	t_token	*tmp;
+
+	tmp = main->token;
+	while (tmp)
+	{
+		token_type(tmp, main);
+		tmp = tmp->next;
+	}
+	ft_no_cmd(main);
+	tmp = main->token;
+	while (tmp)
+	{
+		token_type(tmp, main);
+		tmp = tmp->next;
+	}
+}
 
 t_token	*ft_tokenizer(char *line, t_main *main)
 {
 	int		i;
 	char	**content;
 	char	**tab;
-	t_token	*tmp;
 
 	i = 0;
-	tab = ft_split_ms(line, "<>| ");
+	tab = ft_split_ms(line, "<>|");
 	while (tab[i])
 	{
 		if (ft_strncmp(tab[i], "<<", 2) != 0)
@@ -154,13 +186,8 @@ t_token	*ft_tokenizer(char *line, t_main *main)
 			ft_new_node(&main->token, content);
 		i++;
 	}
-	tmp = main->token;
-	while (tmp)
-	{
-		token_type(tmp, main);
-		tmp = tmp->next;
-	}
-	ft_group_cmd(main->token);
+	ft_find_type(main);
 	ft_sort(main);
+//	clear_token(main);
 	return (main->token);
 }
